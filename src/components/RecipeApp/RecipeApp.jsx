7e18f59f759a0de4a67712e370635d265e2699d8
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-06-10 18:16:43 +0300

    рецепт

--- src/components/RecipeApp/RecipeApp.jsx
+++ src/components/RecipeApp/RecipeApp.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import styles from './RecipeApp.module.scss';
@@ -7,7 +5,7 @@ import ponyGif from './pinkiepie-mlp.gif';
 
 const RecipeApp = () => {
   const [year, setYear] = useState('');
-  const [recipe, setRecipe] = useState('');
+  const [recipe, setRecipe] = useState([]);
   const [error, setError] = useState('');
   const [showAnimation, setShowAnimation] = useState(false);
 
@@ -15,7 +13,14 @@ const RecipeApp = () => {
     setYear(event.target.value);
   };
 
-  const text = '1. У великій місцевості, де зібрано достатньо магічної енергії, наберіть 150мл води; 2. У магічному котлі додайте 2 гр Блакитного Місячного Пилу. Намагайтеся зберегти його ніжну синю кольорову силу. 3.  Обережно влийте 1 гр Вогняного Драконового Соку. Спостерігайте, як він додає різкість та енергію до зілля.'
+  const text = [
+    '1. У великій місцевості, де зібрано достатньо магічної енергії, наберіть 330мл води;',
+    '2. У магічному колі додайте 2 гр Блакитного Місячного Пилу. Намагайтеся зберегти його ніжну синю кольорову силу.',
+    '3. Обережно влийте 1 гр Вогняного Драконового Соку. Спостерігайте, як він додає різкість та енергію до зілля.',
+    '4. Потім додайте 3 гр Золотої Сонячної Роси, даруючи зіллю світлу та теплу ауру.',
+    '5. Гарно змішайте всі інгрідієнти, доки вони не набудуть потужної енергії.',
+    '6. Перелийте зілля в чарівну посудину і підготуйтеся до використання магічного зілля "Колірні чари".'
+  ];
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -25,7 +30,7 @@ const RecipeApp = () => {
       setError('');
       setShowAnimation(true);
     } else {
-      setRecipe('');
+      setRecipe([]);
       setError('Помилка: неправильний рік!');
       setShowAnimation(false);
     }
@@ -45,57 +50,21 @@ const RecipeApp = () => {
         </label>
         <button type="submit" className={styles.button}>Пошук</button>
       </form>
-      {recipe && <p className={styles.recipe}>Рецепт: {recipe}</p>}
+      {recipe.length > 0 && (
+        <ul className={styles.recipe}>
+          {recipe.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ul>
+      )}
       {error && <p className={styles.error}>{error}</p>}
       {showAnimation && (
         <animated.div className={styles.animation} style={animationProps}>
           <img src={ponyGif} alt="Jumping Pony with Stars" className={styles.pony} />
         </animated.div>
-        
-
       )}
     </div>
   );
 };
 
 export default RecipeApp;
-
-
-
-// const RecipeApp = () => {
-//   const [year, setYear] = useState('');
-//   const [recipe, setRecipe] = useState('');
-//   const [error, setError] = useState('');
-
-//   const handleInputChange = (event) => {
-//     setYear(event.target.value);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-
-//     if (year === '2020') {
-//       setRecipe('2 яблука і слива');
-//       setError('');
-//     } else {
-//       setRecipe('');
-//       setError('Помилка: неправильний рік!');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Введіть рік:
-//           <input type="text" value={year} onChange={handleInputChange} />
-//         </label>
-//         <button type="submit">Пошук</button>
-//       </form>
-//       {recipe && <p>Рецепт: {recipe}</p>}
-//       {error && <p>{error}</p>}
-//     </div>
-//   );
-// };
-
-// export default RecipeApp;
